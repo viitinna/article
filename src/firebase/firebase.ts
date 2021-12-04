0c034031218ed6f79602c3b02ac3a5089e2d2cb0
viitinna <inna.hudyma18@gmail.com>
2021-12-04 17:48:34 +0200

    first commit

---
+++ src/firebase/firebase.ts
@@ -0,0 +1,21 @@
+
+import firebase from 'firebase/compat/app';
+import "firebase/auth";
+import "firebase/database";
+
+const config = {
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(config);
+}
+
+export const auth = firebase.auth();
+export const db = firebase.database();
